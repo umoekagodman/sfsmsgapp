437e84fb6b3388df3135290ba0247c3c4a6967b7
umoekagodman <62608298+umoekagodman@users.noreply.github.com>
2025-11-01 16:40:40 +0000

    Update build.gradle.kts

--- sfsmsgapp/Source/android/app/build.gradle.kts
+++ sfsmsgapp/Source/android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "messaging.sngine.app"
+        applicationId = "messaging.sfs.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
