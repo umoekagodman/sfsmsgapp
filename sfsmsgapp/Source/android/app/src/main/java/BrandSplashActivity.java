b40465cb5dcd4dce85fb14cdebb887283ba727bc
umoekagodman <62608298+umoekagodman@users.noreply.github.com>
2025-11-08 09:04:09 +0000

    Update BrandSplashActivity.java

--- sfsmsgapp/Source/android/app/src/main/java/BrandSplashActivity.java
+++ sfsmsgapp/Source/android/app/src/main/java/BrandSplashActivity.java
@@ -26,6 +26,6 @@ public class BrandSplashActivity extends AppCompatActivity {
             startActivity(new Intent(this, MyFlutterActivity.class));
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
             finish();
-        }, 2000);
+        }, 1000);
     }
 }
